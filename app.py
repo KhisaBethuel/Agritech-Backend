cd603acbe842de8e12c7f7712ffdab6dd067daab
Collins <collins.wachira@student.moringaschool.com>
2024-11-20 12:51:09 +0300

    adjusted the seed file

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.environ.get("JWT_SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///agriconnect.db'
+
 
 # Initialize extensions
 db.init_app(app)
